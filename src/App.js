554fd3fe01ddeb1c90024f12fe41200e85f8e0ee
MatthieuTricoire <matthieutricoire@gmail.com>
2023-02-04 10:24:19 +0000

    css done

--- src/App.js
+++ src/App.js
@@ -1,31 +1,51 @@
-// Components Import
-import { useState } from "react";
-import "./App.css";
+//Assets import
+import logo from "./assets/img/logo.png";
 
+// Components Import
+import "./App.css";
+import { useState } from "react";
 import Counter from "./components/Counter";
 
 function App() {
   const [counter, setCounter] = useState([]);
+  const [maxCounters, setMaxCounters] = useState(false);
 
   const addCounter = () => {
     if (counter.length < 3) {
       const newTab = [...counter];
       newTab.push(0);
       setCounter(newTab);
-    } else {
-      alert("max 3 counters");
     }
+    // } else {
+    //   setMaxCounters(!maxCounters);
+    // }
+    if (counter.length === 2) setMaxCounters(!maxCounters);
   };
 
   return (
-    <div className="main">
-      <button onClick={addCounter} className="btn">
-        Add counter
-      </button>
-      {counter.map((elem, index) => {
-        <Counter counter={counter} setCounter={setCounter} index={index} />;
-      })}
-    </div>
+    <>
+      <header className="row">
+        <img className="header__logo" src={logo} alt="Chronomètre" />
+        <h1 className="header__title">React Counter v2</h1>
+      </header>
+
+      <main className="main">
+        <section className="row">
+          <button
+            onClick={addCounter}
+            className={`btn btn--large ${maxCounters && "btn--disabled"}`}
+          >
+            Add counter
+          </button>
+        </section>
+
+        <section className="row">
+          {counter.map((elem, index) => (
+            <Counter counter={counter} setCounter={setCounter} index={index} />
+          ))}
+        </section>
+      </main>
+    </>
   );
 }
 
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -10,15 +10,38 @@ const Counter = ({ counter, setCounter, index }) => {
     newTab[index] = newTab[index] - 1;
     setCounter(newTab);
   };
+
+  const reset = () => {
+    const newTab = [...counter];
+    newTab[index] = 0;
+    setCounter(newTab);
+  };
   return (
-    <div className="counter">
-      <button className="btn counter__btn" onClick={decrement}>
-        -
-      </button>
-      <div className="counter__result">{counter}</div>
-      <button className="btn counter__btn" onClick={increment}>
-        +
-      </button>
+    <div className="counter colum" key={index}>
+      <div className="counter__main row">
+        <button
+          className={`btn counter__btn__action ${
+            counter[index] < 1 && "btn--disabled"
+          }`}
+          onClick={decrement}
+        >
+          -
+        </button>
+        <div className="counter__main__result row">{counter[index]}</div>
+        <button
+          className={`btn counter__btn__action ${
+            counter[index] > 9 && "btn--disabled"
+          }`}
+          onClick={increment}
+        >
+          +
+        </button>
+      </div>
+      <div className="counter__reset row">
+        <button className="btn btn--large" onClick={reset}>
+          Reset
+        </button>
+      </div>
     </div>
   );
 };
